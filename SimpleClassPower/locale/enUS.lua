a3ef345191dcb4faba48151705dcc7e3dad42f3c
Lars Norberg <lars@larsnorbergofficial.com>
2019-03-19 19:12:36 +0100

    working around blizzard's bugged format strings in the back-end

--- SimpleClassPower/locale/enUS.lua
+++ SimpleClassPower/locale/enUS.lua
@@ -8,3 +8,21 @@ L["Disable"] = true
 L["Enabled"] = "|cff00aa00Enabled|r"
 L["Disabled"] = "|cffff0000Disabled|r"
 
+L["Position"] = true
+L["While this window is open, you can drag the anchor around the screen, nudge it with the buttons, or even enter a manual position in the input boxes below."] = true
+
+L["Scale"] = true 
+L["Set the scale of resource points."] = true
+
+L["Color Scheme"] = true 
+L["Here you can select how you wish the resource points to be colored."] = true
+
+L["Power Colors"] = true 
+L["Color the resource points according to the resource type. This is the default setting."] = true
+
+L["Class Colors"] = true 
+L["Color the resource points according to your class."] = true 
+
+L["Custom Color"] = true
+L["Color the resource points in your own manually set color."] = true
+
